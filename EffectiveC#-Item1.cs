be354db781163bb296afa7303e34d6e672b69d39
cpf <cpf@cpfdeMacBook-Pro.local>
2021-08-16 20:46:11 +0800

    item 1 example

---
+++ EffectiveC#-Item1.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+
+namespace csharp_learning
+{
+
+    class Animal { }
+    class Cat : Animal { }
+    class Dog : Animal { }
+
+    public class UnitTest1
+    {
+        Animal CreateAnimal()
+        {
+            return new Dog();
+        }
+
+        [Fact]
+        public void VarShouldNotLoseType()
+        {
+            var animal = CreateAnimal();
+
+            Assert.Equal("Dog", animal.GetType().Name);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -6,3 +6,9 @@ https://bit.ly/3COcYsi
 50 items in Effective C#, 4 groups
 
 Book link: https://www.amazon.com/Effective-Covers-Content-Update-Program-ebook-dp-B01N7C4P8N/dp/B01N7C4P8N
+
+## how to use this repo
+
+Create a unit test file, like EffectiveC#-Item1.cs.
+
+Write some tests to verify what you have learnt in the book.
